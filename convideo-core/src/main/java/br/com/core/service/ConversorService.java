7aa272a0fb66c9201a5787fd3110ba85cbe281f2
coutom <michael.bhz@gmail.com>
2013-06-09 18:00:22 -0300

    commit

--- convideo-core/src/main/java/br/com/core/service/ConversorService.java
+++ convideo-core/src/main/java/br/com/core/service/ConversorService.java
@@ -13,6 +13,14 @@ public class ConversorService implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 4871258892762831435L;
+
+	/**
+	 * Converter o arquivo do formato DV para formato flash valido.
+	 */
+	public static boolean DvToFlash() {
+		return true;
+		
+	}
 	
 	
 
--- convideo-core/src/main/java/br/com/core/service/RemoteService.java
+++ convideo-core/src/main/java/br/com/core/service/RemoteService.java
@@ -3,7 +3,13 @@
  */
 package br.com.core.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author michael
@@ -15,13 +21,27 @@ public class RemoteService implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -2411034827445498627L;
+		
 	
 	/**
 	 * Obter aquivo de entrada de acordo com URL informada.
+	 * @param arquivoEntrada 
 	 * @return
 	 */
-	public static String obterArquivoEntrada(){
-		return "arquivo";
+	public static boolean obterArquivoEntrada(String arquivoEntrada){
+		try {
+			URL url = new URL(arquivoEntrada);
+			String nomeArquivoLocal = url.getPath();
+			InputStream is = url.openStream(); 	
+			return true;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+		
 	}
+
 
 }
